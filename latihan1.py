00fa29ffa6745f22fa990b2c89ea4b2c88663aba
legita112 <143859848+legita112@users.noreply.github.com>
2023-09-02 10:59:40 +0700

    Add files via upload
    first file

---
+++ latihan1.py
@@ -0,0 +1,31 @@
+nama_saya = str(input("Masukan nama anda = "))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = int(input("Masukan umur anda = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("Masukan IPK anda = "))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input("Sudah menikah? = "))
+print("menikah?",menikah, "bertipe", type(menikah))
+
+# matkul_diambil = ["alpro", "sig"]
+# print("Matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+# matkul_diambil2 = ("web", "pemvis")
+# print("Matkul Lain : ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+# matkul_diambil3 = {"Alpro", "Alpro", "pemweb", "Data Science", "Data Scince"}
+# print("Matkul Lain-Lain : ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+# print()
+# print()
+
+# print(2 + 2)
+# print(5 - 2)
+# print(3 * 5)
+# print(27 / 4)
+# print(27 // 4)
+# print(27 % 4)
+
